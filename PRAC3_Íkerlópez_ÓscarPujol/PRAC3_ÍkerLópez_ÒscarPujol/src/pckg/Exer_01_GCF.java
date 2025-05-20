d71a88a0bd309d6843508c823ed0ae1f0a198f64
Iker.dev <129156276+Ikerlopez13@users.noreply.github.com>
2025-05-20 10:05:14 +0200

    Update Exer_01_GCF.java

--- PRAC3_Íkerlópez_ÓscarPujol/PRAC3_ÍkerLópez_ÒscarPujol/src/pckg/Exer_01_GCF.java
+++ PRAC3_Íkerlópez_ÓscarPujol/PRAC3_ÍkerLópez_ÒscarPujol/src/pckg/Exer_01_GCF.java
@@ -9,12 +9,34 @@ public class Exer_01_GCF {
 		/* COMPLETE */
 		/* Write here a program that generates 100000 pairs of numbers
 		between [1, 5000] to check that the function calculates the GCF.*/
+		Random r = new Random();
+		for(int i = 0; i<100000; i++){
+			int x = r.nextInt(1, 5001);
+			int y = r.nextInt(1, 5001);
 
+			System.out.println("MCD: "+ MCD( x, y) +" iterativeGCF: "+ iterativeGCF(x,y));
+		}
 	}
 	
 	
 	/* COMPLETE */
 	// write here a recursive function that calculates the GCF using Dijkstra's algorithm
+
+
+	public static int MCD(int x, int y){
+		if(x==y){
+			return x;
+		} else{
+			int max = x;
+			int min = y;
+			if(y>x) {
+				max = y;
+				min = x;
+			}
+			int r = max-min;
+			return MCD(r, min);
+		}
+	}
 
 
 	/*DO NOT MODIFY CODE BELOW*/
